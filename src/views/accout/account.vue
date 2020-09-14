a3c4c7fe34967d1150467d309f0cb29b52fc92e5
denghua <huadeng@ddkt365.com>
2020-09-14 11:33:00 +0800

    feat: 开户页版本撤回

--- src/views/accout/account.vue
+++ src/views/accout/account.vue
@@ -12,7 +12,7 @@
       <input class="phone-input" v-model="phone" type="text" placeholder="请输入手机号码">
     </div>
 
-    <div class="phone-desc" v-if="isShowTips">
+    <div class="phone-desc">
       <p>请输入正确手机号</p>
       <p>仅作<span>后期奖励发放和专属服务凭证</span></p>
     </div>
@@ -29,8 +29,7 @@ export default {
   data() {
     return {
       securitiesName: this.$route.query.name || '',
-      phone: '',
-      isShowTips: false
+      phone: ''
     }
   },
   beforeRouteLeave(to, from, next) { // 离开时
@@ -64,7 +63,6 @@ export default {
           msg: '请输入正确的电话号码',
           type: 'warning'
         })
-        this.isShowTips = true
         return false
       }
 
