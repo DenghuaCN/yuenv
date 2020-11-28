47ae50d5a377bc5d7c259137f45eaa6e76f12610
denghua <huadeng@ddkt365.com>
2020-11-28 20:16:09 +0800

    feat: nginx gzip 配置

--- config/index.js
+++ config/index.js
@@ -40,7 +40,6 @@ module.exports = {
         // https://vue-loader.vuejs.org/en/options.html#cachebusting
         cacheBusting: true,
 
-        // cssSourceMap: process.env.NODE_ENV === 'production' ? false : true
         cssSourceMap: false
     },
 
@@ -58,7 +57,6 @@ module.exports = {
          * Source Maps
          */
 
-        // productionSourceMap: process.env.NODE_ENV === 'production' ? false : true,
         productionSourceMap: false,
         // https://webpack.js.org/configuration/devtool/#production
         devtool: '#source-map',
@@ -67,8 +65,7 @@ module.exports = {
         // Surge or Netlify already gzip all static assets for you.
         // Before setting to `true`, make sure to:
         // npm install --save-dev compression-webpack-plugin
-        productionGzip: false,
-        productionGzipExtensions: ['js', 'css'],
+        productionGzip: true,
 
         // Run the build command with an extra argument to
         // View the bundle analyzer report after build finishes:
