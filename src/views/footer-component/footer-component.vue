1ad9140f9e135f716e1d32c1fd78bb891025258d
denghua <huadeng@ddkt365.com>
2020-12-01 01:34:32 +0800

    feat: 压缩图片, 上正式环境,Gzip问题未解决

--- src/views/footer-component/footer-component.vue
+++ src/views/footer-component/footer-component.vue
@@ -1,35 +1,35 @@
 <template>
-  <div class="footer">
-      <button class="tips-button">温馨提示</button>
-      <p class="tips-p">为避免无效奖励，<br/>入金需满足各券商<em>最低金额要求</em>，并在账户<em>留存满1月</em>。</p>
-      <img class="qr-img" src="../../assets/img/yuenv/qr.png">
-      <p class="wx-kefu">越小港-专属客服微信</p>
-      <p class="wx-num">(cxm6688789)</p>
-      <div class="bottom-desc">
-        <img src="../../assets/img/yuenv/left-line.png">
-        <p>越女事务所-测试环境</p>
-        <img src="../../assets/img/yuenv/right-line.png">
-      </div>
-  </div>
+    <div class="footer">
+        <button class="tips-button">温馨提示</button>
+        <p class="tips-p">为避免无效奖励，<br />入金需满足各券商<em>最低金额要求</em>，并在账户<em>留存满1月</em>。</p>
+        <img class="qr-img" src="../../assets/img/yuenv/qr.png">
+        <p class="wx-kefu">越小港-专属客服微信</p>
+        <p class="wx-num">(cxm6688789)</p>
+        <div class="bottom-desc">
+            <img src="../../assets/img/yuenv/left-line.png">
+            <p>港哥事务所</p>
+            <img src="../../assets/img/yuenv/right-line.png">
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'footer-component',
-  components: {
-  },
-  data() {
-    return {
+    name: 'footer-component',
+    components: {
+    },
+    data() {
+        return {
 
+        }
+    },
+    computed: {
+    },
+    mounted() {
     }
-  },
-  computed: {
-  },
-  mounted() {
-  }
 }
 </script>
 
 <style lang='less' scoped>
-@import './footer-component.less';
+@import "./footer-component.less";
 </style>
