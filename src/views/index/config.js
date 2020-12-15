b2d8d5e19349587d93810dda4c8188297d10a86e
denghua <huadeng@ddkt365.com>
2020-12-15 12:00:16 +0800

    feat: 华泰链接更换

--- src/views/index/config.js
+++ src/views/index/config.js
@@ -1,219 +1,218 @@
 export const config = {
-  dataList: [
-    {
-      securitiesName: '富途证券',
-      iconSrc: require('../../assets/img/yuenv/fu-tu.png'),
-      iconDesc: ['入金奖励', '10倍融资', '独立暗盘'],
-      mainDescList: [
-        '开户即可获得<em>180天港股免佣</em>',
-        '首次入金<em>≥2万港币</em>，可获得<em>1次抽奖+基金收益券</em>：100%抽中130港币小米股票，最高1000美金特斯拉股票。'
-      ],
-      activeDesc: [
-        '现金打新50，融资打新100，10倍杠杆，利率2-3%',
-        '支持fps，支持民生、招行、工行银证转账，独立暗盘，可1手融，可市值打新'
-      ],
-      activeArrow: true,
-      showActiveDesc: false,
-      url: 'https://www.futuhk.com/reg?channel=322&subchannel=1'
-    },
-    {
-      securitiesName: '老虎证券',
-      iconSrc: require('../../assets/img/yuenv/tiger.png'),
-      iconDesc: ['入金奖励', '20倍融资', '市值打新'],
-      mainDescList: [
-        '开通<em>“综合账户”，首次入金≥2.4W港币，送1股阿里巴巴港股（价值约280港币）。</em>'
-      ],
-      activeDesc: [
-        '开户完需在“设置-账户管理”进行个人信息验证',
-        '无现金打新，融资打新100，15-20倍杠杆，利率2-3%',
-        '标准户<em>支持fps，无银证转账</em>，有暗盘，不可1手融，可市值打新',
-        '交易佣金：0.029%+15元平台费',
-        '卖出股票资金可实时用于打新'
-      ],
-      activeArrow: true,
-      showActiveDesc: false,
-      url: 'https://www.itiger.com/activity/market/2019/stock-ali/?noPromotion=1&invite=TG001'
-    },
-    {
-      securitiesName: '华盛证券',
-      smallSecuritiesName: '华盛通',
-      iconSrc: require('../../assets/img/yuenv/hua-sheng.png'),
-      iconDesc: ['入金奖励', '10倍融资', '市值打新'],
-      mainDescList: [
-        '首次入金<em>≥1.3万港币</em>，送<em>900元佣金卡+250元</em>融资打新券。'
-      ],
-      activeDesc: [
-        '现金打新50，融资打新100，10倍杠杆，利率2-3%',
-        '<em>支持fps，支持民生、招行、永隆银证转账</em>，辉立暗盘，可1手融，不可市值打新',
-        '交易佣金：0.03%（最低3元）+平台费15元/笔，暗盘同',
-        '卖出股票资金可实时用于打新'
-      ],
-      activeArrow: true,
-      showActiveDesc: false,
-      url: 'https://www.vbkr.com/hd/marketing/2019/scnl/ZDRpMHI3cDQzYjJl/register'
-    },
-    {
-      securitiesName: '艾德证券',
-      smallSecuritiesName: '艾德一站通',
-      iconSrc: require('../../assets/img/yuenv/ai-de.png'),
-      iconDesc: ['入金奖励', '20倍融资', '免佣优惠'],
-      mainDescList: [
-        '首次<em>入金≥1.1万</em>港币，送180天港股免佣（入金次月生效）。',
-        '<em>现金打新0费用</em>，赠送终身免费港股实时行情'
-      ],
-      activeDesc: [
-        '现金打新0，融资打新100，10~20倍杠杆',
-        '支持fps，无银证转账，辉立暗盘，可1手融，不可市值打新',
-        '交易佣金：免佣期间不收佣金，仅收12元平台费，暗盘万5，最低30',
-        '卖出股票资金可实时用于打新'
-      ],
-      activeArrow: true,
-      showActiveDesc: false,
-      url: 'https://aosh5.eddidapp.com/?rf=Q1kwMDU6RURBQQ=='
-    },
-    {
-      securitiesName: '华泰国际',
-      smallSecuritiesName: '涨乐全球通',
-      iconSrc: require('../../assets/img/yuenv/hua-tai.png'),
-      iconDesc: ['入金奖励', '33倍融资', '市值打新'],
-      mainDescList: [
-        '开户送<em>南方恒生科技+1个月V2会员</em>（价值78港币）',
-        '首次<em>入金≥2万港币</em>，再送南方恒生科技+1股港股阿里巴巴（价值300港币）'
-      ],
-      activeDesc: [
-        '会员现金打新0，融资打新0，10-33倍杠杆，利率3-4%',
-        '支持fps，电子暗盘，融资需一手本金，可市值打新',
-        '交易佣金：会员0佣金+0平台费，非会员0.029%（最低2.9元）+平台费15元/笔，暗盘0.15%',
-        '卖出股票资金可实时用于打新'
-      ],
-      activeArrow: true,
-      showActiveDesc: false,
-      url: 'https://gb.zhangle.com/views/new-customer-gift/index.htm?ciphertext=349a5ed7d240b16cc386c41f392e8b2baa40d0b47222759a2b07f5ac39ce250edbdf0c6d7af0d00c8b93a7d997b5794719f4ff665b9147b285639a7aa4822d58'
-    },
-    {
-      securitiesName: '东财国际证券',
-      iconSrc: require('../../assets/img/yuenv/dong-cai.png'),
-      iconDesc: ['现金0费用', '20倍融资', '电子暗盘'],
-      mainDescList: [
-        '<em>首次入金≥2W港币</em>，现金打新25港币费用。'
-      ],
-      activeDesc: [
-        '融资打新100，10-20倍杠杆，利率2-3%',
-        '支持<em>fps，支持民生、招行、工银银证转账</em>，辉立暗盘，不可1手融，不可市值打新',
-        '交易佣金：0.025%（最低5元）+平台费15元/笔，暗盘0.025%（最低3元）+平台费15元/笔',
-        '卖出股票资金可实时用于打新'
-      ],
-      activeArrow: true,
-      showActiveDesc: false,
-      url: 'https://acttg.eastmoney.com/pub/zwtg_kh_act_grqdhz_01_01_39_0'
-    },
-    {
-      securitiesName: '雪盈证券',
-      iconSrc: require('../../assets/img/yuenv/xue-ying.png'),
-      iconDesc: ['入金奖励', '10倍融资', '市值打新'],
-      mainDescList: [
-        '<em>入金≥3.5w港币</em>，送8美元返佣卡*2，48港币返佣卡*2，最高价值470港币。',
-        '<em>入金≥3.5w港币</em>，60天内交易5次可领80港币现金，最高666港币现金。'
-      ],
-      activeDesc: [
-        '港股无现金打新，融资打新100，热门股10倍杠杆， 利率2-3%',
-        '不支持fps，无银证转账，无暗盘，不可1手融，可市值打新',
-        '交易佣金：0.03%（最低3元）+平台费15元/笔',
-        '卖出股票资金可实时用于打新'
-      ],
-      activeArrow: true,
-      showActiveDesc: false,
-      url: 'https://www.snowballsecurities.com/activity/open/open-v10?r=50001006155'
-    },
-    {
-      securitiesName: '方德证券',
-      smallSecuritiesName: '方德港美股',
-      iconSrc: require('../../assets/img/yuenv/fang-de.png'),
-      iconDesc: ['预约额度', '30倍融资', '现金免费'],
-      mainDescList: [
-        '<em>首次入金≥1W</em>，且交易1次并留存1个月，可享<em>现金打新永久免费</em>。'
-      ],
-      activeDesc: [
-        '融资打新120元/笔，10-30倍杠杆，利率5%',
-        '支持fps，无银证转账，电子暗盘，可1手融，不可市值打新',
-        '交易佣金：0.25%（最低100元），无平台费，暗盘同',
-        '卖出股票资金可实时用于打新'
-      ],
-      activeArrow: true,
-      showActiveDesc: false,
-      url: 'https://open-fd.forthright-sec.com/v9/index.html?source=BGNC&empId=0'
-    },
-    {
-      securitiesName: '富元证券',
-      smallSecuritiesName: '玖富犇犇',
-      iconSrc: require('../../assets/img/yuenv/jiu-fu.png'),
-      iconDesc: ['入金奖励', '10倍融资', '市值打新'],
-      mainDescList: [
-        '开户翻牌1次，首笔入金到账满<em>1.2万-10万</em>，翻牌3-10次。',
-        '首次交易任意正股翻牌1次<br/>随机奖品：<em>iPhone12 Pro、AirPods Pro、现金红包、铁塔股票、平安股票、美团股票、港股免佣券</em>。'
-      ],
-      activeDesc: [
-        '现金打新10，融资打新59-99港币，10倍杠杆，利率2.88%',
-        '支持fps，支持<em>民生银证转账</em>，辉立暗盘，可取资金超5000可1手融，不可市值打新',
-        '交易佣金：0.029%（最低15元），无平台费，暗盘0.05%（最低15元）',
-        '卖出股票资金T+2交收后方可用于打新'
-      ],
-      activeArrow: true,
-      showActiveDesc: false,
-      url: 'https://sns.9fstock.com/sunline/main/index.html#/community/accounts/login?invUserId=1&channelId=509&utm_source=3rd-zhuce&utm_medium=YNSWS'
-    },
-    {
-      securitiesName: '盈立证券',
-      iconSrc: require('../../assets/img/yuenv/ying-li.png'),
-      iconDesc: ['入金奖励', '10倍融资', '辉立暗盘'],
-      mainDescList: [
-        '开户送<em>30天港股免佣+永久美股免佣+180天高级行情</em>。',
-        '首次<em>入金2-5万</em>，享3次抽股票卡机会。',
-        '首次<em>入金5-10万</em>，交易额3万，送400元现金红包。',
-        '首次<em>入金10万以上</em>，交易额5万，送500元现金红包+1张20倍融资打新卡。'
-      ],
-      activeDesc: [
-        '现金打新18，融资打新99，10倍杠杆，利率2-3%',
-        '支持fps，无银证转账，辉立暗盘，可1手融，不可市值打新',
-        '交易佣金：0.08%（最低8元），无平台费，暗盘0.08%（最低12元）',
-        '卖出股票资金可实时用于打新'
-      ],
-      activeArrow: true,
-      showActiveDesc: false,
-      url: ' https://m.yxzq.com/webapp/marketing/jump-register.html?register-ct=ic&register-cid=788&ICode=266233#/register'
-    },
-    {
-      securitiesName: '佳兆业证券',
-      iconSrc: require('../../assets/img/yuenv/jia-zhao-ye.png'),
-      iconDesc: ['入金奖励', '10倍融资', '电话暗盘'],
-      mainDescList: [
-        '首次<em>入金≥1.1万港币</em>并放满30天，且入金后90天内交易1次，送100元现金红包'
-      ],
-      activeDesc: [
-        '现金打新50，融资打新80，10倍杠杆，融资利率2-3%',
-        '支持fps，支持<em>民生银证转账</em>，电话暗盘，可1手融，不可市值打新',
-        '交易佣金：0.05%（最低15元），无平台费，暗盘120元起',
-        '卖出股票资金可实时用于打新'
-      ],
-      activeArrow: true,
-      showActiveDesc: false,
-      url: 'https://www.kaisasecurities.com/Member/Register?channel=ADWMSWZX&YYM=32'
-    },
-    {
-      securitiesName: '尊嘉金融',
-      iconSrc: require('../../assets/img/yuenv/zun-jia.png'),
-      iconDesc: ['入金奖励', '10倍融资', '0交易佣金'],
-      mainDescList: [
-        '首次<em>入金≥1W</em>港币，30天内交易1次，享受<em>0佣金+平台费1元/笔</em>。'
-      ],
-      activeDesc: [
-        '现金打新5，融资打新36.9（热门100），10倍杠杆，利率2-3%',
-        '支持fps，无银证转账，辉立暗盘0.05%（最低11.9元），可1手融，不可市值打新',
-        '卖出股票资金可实时用于打新'
-      ],
-      activeArrow: true,
-      showActiveDesc: false,
-      url: 'https://accountapi.investassistant.com/miningaccount/accounth5/regist?activity=&channel_open=pEYQVy3O3wvYOxwbxpQX2A..&hmsr=qdwy&hmpl=&hmcu=&hmkw=&hmci='
-    }
-  ]
+    dataList: [{
+            securitiesName: '富途证券',
+            iconSrc: require('../../assets/img/yuenv/fu-tu.png'),
+            iconDesc: ['入金奖励', '10倍融资', '独立暗盘'],
+            mainDescList: [
+                '开户即可获得<em>180天港股免佣</em>',
+                '首次入金<em>≥2万港币</em>，可获得<em>1次抽奖+基金收益券</em>：100%抽中130港币小米股票，最高1000美金特斯拉股票。'
+            ],
+            activeDesc: [
+                '现金打新50，融资打新100，10倍杠杆，利率2-3%',
+                '支持fps，支持民生、招行、工行银证转账，独立暗盘，可1手融，可市值打新'
+            ],
+            activeArrow: true,
+            showActiveDesc: false,
+            url: 'https://www.futuhk.com/reg?channel=322&subchannel=1'
+        },
+        {
+            securitiesName: '老虎证券',
+            iconSrc: require('../../assets/img/yuenv/tiger.png'),
+            iconDesc: ['入金奖励', '20倍融资', '市值打新'],
+            mainDescList: [
+                '开通<em>“综合账户”，首次入金≥2.4W港币，送1股阿里巴巴港股（价值约280港币）。</em>'
+            ],
+            activeDesc: [
+                '开户完需在“设置-账户管理”进行个人信息验证',
+                '无现金打新，融资打新100，15-20倍杠杆，利率2-3%',
+                '标准户<em>支持fps，无银证转账</em>，有暗盘，不可1手融，可市值打新',
+                '交易佣金：0.029%+15元平台费',
+                '卖出股票资金可实时用于打新'
+            ],
+            activeArrow: true,
+            showActiveDesc: false,
+            url: 'https://www.itiger.com/activity/market/2019/stock-ali/?noPromotion=1&invite=TG001'
+        },
+        {
+            securitiesName: '华盛证券',
+            smallSecuritiesName: '华盛通',
+            iconSrc: require('../../assets/img/yuenv/hua-sheng.png'),
+            iconDesc: ['入金奖励', '10倍融资', '市值打新'],
+            mainDescList: [
+                '首次入金<em>≥1.3万港币</em>，送<em>900元佣金卡+250元</em>融资打新券。'
+            ],
+            activeDesc: [
+                '现金打新50，融资打新100，10倍杠杆，利率2-3%',
+                '<em>支持fps，支持民生、招行、永隆银证转账</em>，辉立暗盘，可1手融，不可市值打新',
+                '交易佣金：0.03%（最低3元）+平台费15元/笔，暗盘同',
+                '卖出股票资金可实时用于打新'
+            ],
+            activeArrow: true,
+            showActiveDesc: false,
+            url: 'https://www.vbkr.com/hd/marketing/2019/scnl/ZDRpMHI3cDQzYjJl/register'
+        },
+        {
+            securitiesName: '艾德证券',
+            smallSecuritiesName: '艾德一站通',
+            iconSrc: require('../../assets/img/yuenv/ai-de.png'),
+            iconDesc: ['入金奖励', '20倍融资', '免佣优惠'],
+            mainDescList: [
+                '首次<em>入金≥1.1万</em>港币，送180天港股免佣（入金次月生效）。',
+                '<em>现金打新0费用</em>，赠送终身免费港股实时行情'
+            ],
+            activeDesc: [
+                '现金打新0，融资打新100，10~20倍杠杆',
+                '支持fps，无银证转账，辉立暗盘，可1手融，不可市值打新',
+                '交易佣金：免佣期间不收佣金，仅收12元平台费，暗盘万5，最低30',
+                '卖出股票资金可实时用于打新'
+            ],
+            activeArrow: true,
+            showActiveDesc: false,
+            url: 'https://aosh5.eddidapp.com/?rf=Q1kwMDU6RURBQQ=='
+        },
+        {
+            securitiesName: '华泰国际',
+            smallSecuritiesName: '涨乐全球通',
+            iconSrc: require('../../assets/img/yuenv/hua-tai.png'),
+            iconDesc: ['入金奖励', '33倍融资', '市值打新'],
+            mainDescList: [
+                '开户送<em>南方恒生科技+1个月V2会员</em>（价值78港币）',
+                '首次<em>入金≥2万港币</em>，再送南方恒生科技+1股港股阿里巴巴（价值300港币）'
+            ],
+            activeDesc: [
+                '会员现金打新0，融资打新0，10-33倍杠杆，利率3-4%',
+                '支持fps，电子暗盘，融资需一手本金，可市值打新',
+                '交易佣金：会员0佣金+0平台费，非会员0.029%（最低2.9元）+平台费15元/笔，暗盘0.15%',
+                '卖出股票资金可实时用于打新'
+            ],
+            activeArrow: true,
+            showActiveDesc: false,
+            url: 'https://gb.zhangle.com/views/new-customer-gift/index.htm?branch=HTFH-1211841658&bd=4b6618553001c4c567ab966736248bbb3f07e60b67fefbc67b93b6407b765387'
+        },
+        {
+            securitiesName: '东财国际证券',
+            iconSrc: require('../../assets/img/yuenv/dong-cai.png'),
+            iconDesc: ['现金0费用', '20倍融资', '电子暗盘'],
+            mainDescList: [
+                '<em>首次入金≥2W港币</em>，现金打新25港币费用。'
+            ],
+            activeDesc: [
+                '融资打新100，10-20倍杠杆，利率2-3%',
+                '支持<em>fps，支持民生、招行、工银银证转账</em>，辉立暗盘，不可1手融，不可市值打新',
+                '交易佣金：0.025%（最低5元）+平台费15元/笔，暗盘0.025%（最低3元）+平台费15元/笔',
+                '卖出股票资金可实时用于打新'
+            ],
+            activeArrow: true,
+            showActiveDesc: false,
+            url: 'https://acttg.eastmoney.com/pub/zwtg_kh_act_grqdhz_01_01_39_0'
+        },
+        {
+            securitiesName: '雪盈证券',
+            iconSrc: require('../../assets/img/yuenv/xue-ying.png'),
+            iconDesc: ['入金奖励', '10倍融资', '市值打新'],
+            mainDescList: [
+                '<em>入金≥3.5w港币</em>，送8美元返佣卡*2，48港币返佣卡*2，最高价值470港币。',
+                '<em>入金≥3.5w港币</em>，60天内交易5次可领80港币现金，最高666港币现金。'
+            ],
+            activeDesc: [
+                '港股无现金打新，融资打新100，热门股10倍杠杆， 利率2-3%',
+                '不支持fps，无银证转账，无暗盘，不可1手融，可市值打新',
+                '交易佣金：0.03%（最低3元）+平台费15元/笔',
+                '卖出股票资金可实时用于打新'
+            ],
+            activeArrow: true,
+            showActiveDesc: false,
+            url: 'https://www.snowballsecurities.com/activity/open/open-v10?r=50001006155'
+        },
+        {
+            securitiesName: '方德证券',
+            smallSecuritiesName: '方德港美股',
+            iconSrc: require('../../assets/img/yuenv/fang-de.png'),
+            iconDesc: ['预约额度', '30倍融资', '现金免费'],
+            mainDescList: [
+                '<em>首次入金≥1W</em>，且交易1次并留存1个月，可享<em>现金打新永久免费</em>。'
+            ],
+            activeDesc: [
+                '融资打新120元/笔，10-30倍杠杆，利率5%',
+                '支持fps，无银证转账，电子暗盘，可1手融，不可市值打新',
+                '交易佣金：0.25%（最低100元），无平台费，暗盘同',
+                '卖出股票资金可实时用于打新'
+            ],
+            activeArrow: true,
+            showActiveDesc: false,
+            url: 'https://open-fd.forthright-sec.com/v9/index.html?source=BGNC&empId=0'
+        },
+        {
+            securitiesName: '富元证券',
+            smallSecuritiesName: '玖富犇犇',
+            iconSrc: require('../../assets/img/yuenv/jiu-fu.png'),
+            iconDesc: ['入金奖励', '10倍融资', '市值打新'],
+            mainDescList: [
+                '开户翻牌1次，首笔入金到账满<em>1.2万-10万</em>，翻牌3-10次。',
+                '首次交易任意正股翻牌1次<br/>随机奖品：<em>iPhone12 Pro、AirPods Pro、现金红包、铁塔股票、平安股票、美团股票、港股免佣券</em>。'
+            ],
+            activeDesc: [
+                '现金打新10，融资打新59-99港币，10倍杠杆，利率2.88%',
+                '支持fps，支持<em>民生银证转账</em>，辉立暗盘，可取资金超5000可1手融，不可市值打新',
+                '交易佣金：0.029%（最低15元），无平台费，暗盘0.05%（最低15元）',
+                '卖出股票资金T+2交收后方可用于打新'
+            ],
+            activeArrow: true,
+            showActiveDesc: false,
+            url: 'https://sns.9fstock.com/sunline/main/index.html#/community/accounts/login?invUserId=1&channelId=509&utm_source=3rd-zhuce&utm_medium=YNSWS'
+        },
+        {
+            securitiesName: '盈立证券',
+            iconSrc: require('../../assets/img/yuenv/ying-li.png'),
+            iconDesc: ['入金奖励', '10倍融资', '辉立暗盘'],
+            mainDescList: [
+                '开户送<em>30天港股免佣+永久美股免佣+180天高级行情</em>。',
+                '首次<em>入金2-5万</em>，享3次抽股票卡机会。',
+                '首次<em>入金5-10万</em>，交易额3万，送400元现金红包。',
+                '首次<em>入金10万以上</em>，交易额5万，送500元现金红包+1张20倍融资打新卡。'
+            ],
+            activeDesc: [
+                '现金打新18，融资打新99，10倍杠杆，利率2-3%',
+                '支持fps，无银证转账，辉立暗盘，可1手融，不可市值打新',
+                '交易佣金：0.08%（最低8元），无平台费，暗盘0.08%（最低12元）',
+                '卖出股票资金可实时用于打新'
+            ],
+            activeArrow: true,
+            showActiveDesc: false,
+            url: ' https://m.yxzq.com/webapp/marketing/jump-register.html?register-ct=ic&register-cid=788&ICode=266233#/register'
+        },
+        {
+            securitiesName: '佳兆业证券',
+            iconSrc: require('../../assets/img/yuenv/jia-zhao-ye.png'),
+            iconDesc: ['入金奖励', '10倍融资', '电话暗盘'],
+            mainDescList: [
+                '首次<em>入金≥1.1万港币</em>并放满30天，且入金后90天内交易1次，送100元现金红包'
+            ],
+            activeDesc: [
+                '现金打新50，融资打新80，10倍杠杆，融资利率2-3%',
+                '支持fps，支持<em>民生银证转账</em>，电话暗盘，可1手融，不可市值打新',
+                '交易佣金：0.05%（最低15元），无平台费，暗盘120元起',
+                '卖出股票资金可实时用于打新'
+            ],
+            activeArrow: true,
+            showActiveDesc: false,
+            url: 'https://www.kaisasecurities.com/Member/Register?channel=ADWMSWZX&YYM=32'
+        },
+        {
+            securitiesName: '尊嘉金融',
+            iconSrc: require('../../assets/img/yuenv/zun-jia.png'),
+            iconDesc: ['入金奖励', '10倍融资', '0交易佣金'],
+            mainDescList: [
+                '首次<em>入金≥1W</em>港币，30天内交易1次，享受<em>0佣金+平台费1元/笔</em>。'
+            ],
+            activeDesc: [
+                '现金打新5，融资打新36.9（热门100），10倍杠杆，利率2-3%',
+                '支持fps，无银证转账，辉立暗盘0.05%（最低11.9元），可1手融，不可市值打新',
+                '卖出股票资金可实时用于打新'
+            ],
+            activeArrow: true,
+            showActiveDesc: false,
+            url: 'https://accountapi.investassistant.com/miningaccount/accounth5/regist?activity=&channel_open=pEYQVy3O3wvYOxwbxpQX2A..&hmsr=qdwy&hmpl=&hmcu=&hmkw=&hmci='
+        }
+    ]
 }
